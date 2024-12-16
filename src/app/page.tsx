7440cb62cff9796d34fa639a2a2426e4f837365e
Gulshan-koondhar <153608541+Gulshan-koondhar@users.noreply.github.com>
2024-12-16 13:59:39 +0500

    added responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 export default function Home() {
   return (
     <div className="max-w-screen-md mx-auto py-20">
-      <div className="grid grid-cols-4 grid-rows-4 gap-2 h-[600px]">
-        <div className="col-span-1 row-span-4 bg-yellow-400  p-4">Nav</div>
-        <div className="col-span-3 row-span-1 bg-yellow-400 p-4">Header</div>
-        <div className="col-span-2 row-span-2 bg-yellow-400 p-4">Article</div>
-        <div className="col-span-1 row-span-2 bg-yellow-400 p-4">Ads</div>
-        <div className="col-span-3 row-span-1 bg-yellow-400 p-4">Footer</div>
+      <div className="grid grid-cols-4 grid-rows-4 gap-2">
+        <div className="col-span-4 row-span-1 sm:col-span-1 sm:row-span-4 bg-yellow-400  p-4">
+          Nav
+        </div>
+        <div className="col-span-4 sm:col-span-3 row-span-1 bg-yellow-400 p-4">
+          Header
+        </div>
+        <div className="col-span-4 row-span-1 sm:col-span-2 sm:row-span-2 bg-yellow-400 p-4">
+          Article
+        </div>
+        <div className="col-span-4 row-span-1 sm:col-span-1 sm:row-span-2 bg-yellow-400 p-4">
+          Ads
+        </div>
+        <div className="col-span-4 sm:col-span-3 row-span-1 bg-yellow-400 p-4">
+          Footer
+        </div>
       </div>
     </div>
   );
